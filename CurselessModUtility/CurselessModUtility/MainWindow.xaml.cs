d215d322e8ba80e604087d10ee609758e2358dd0
Tom 2 <theemeraldtrack@gmail.com>
2022-03-12 11:50:08 +0000

    Added project, with list view and item adding by Ctrl-V

---
+++ CurselessModUtility/CurselessModUtility/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using CurselessModUtility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CurselessModUtility
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Console.WriteLine("Start");
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.V && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+            {
+                var url = Clipboard.GetText();
+
+                if (!Uri.IsWellFormedUriString(url, UriKind.Absolute)) return;
+
+                AddItemByURL(url);
+            }
+        }
+
+        private void AddItemByURL(string url)
+        {
+            var item = new ModListItem
+            {
+                Url = url
+            };
+
+            if (listView.Items.Contains(item)) return;
+
+            listView.Items.Add(item);
+        }
+    }
+}
---
+++ CurselessModUtility/CurselessModUtility/Models/ModListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurselessModUtility.Models
+{
+    internal class ModListItem
+    {
+        public string Url { get; set; }
+        public string? Name { get; set; }
+        public string? Id { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ModListItem other && Url == other.Url;
+        }
+    }
+}
